b0402e4b648bca4fbe010c828d1c1f95487bb328
Bradley Boehmke <bradleyboehmke@gmail.com>
2016-12-27 14:01:00 -0500

    tracking packages leveraged

---
+++ The R Programming Environment/01-packages.R
@@ -0,0 +1,8 @@
+install.packages(c("choroplethr", "choroplethrMaps", "data.table",
+                   "datasets", "devtools", "dlnm", "dplyr", "faraway", "forcats",
+                   "GGally", "ggmap", "ggplot2", "ggthemes", "ghit", "GISTools",
+                   "grid", "gridExtra", "httr", "knitr", "leaflet", "lubridate",
+                   "magrittr", "methods", "microbenchmark", "package", "pander",
+                   "plotly", "profvis", "pryr", "purrr", "rappdirs", "raster",
+                   "RColorBrewer", "readr", "rmarkdown", "sp", "stats", "stringr",
+                   "testthat", "tidyr", "tidyverse", "tigris", "titanic", "viridis"))
